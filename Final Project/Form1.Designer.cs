1b87572e8914e685dc5caaedf95f6182228ce21d
vorya arjomand <vorya_4444@yahoo.com>
2016-05-29 17:20:26 +0430

    final project

---
+++ Final Project/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Final_Project
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnCrawl = new System.Windows.Forms.Button();
+            this.btnParser = new System.Windows.Forms.Button();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.dataGridView1.Size = new System.Drawing.Size(845, 373);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnCrawl
+            // 
+            this.btnCrawl.Location = new System.Drawing.Point(782, 404);
+            this.btnCrawl.Name = "btnCrawl";
+            this.btnCrawl.Size = new System.Drawing.Size(75, 23);
+            this.btnCrawl.TabIndex = 1;
+            this.btnCrawl.Text = "Crawling";
+            this.btnCrawl.UseVisualStyleBackColor = true;
+            this.btnCrawl.Click += new System.EventHandler(this.btnCrawl_Click);
+            // 
+            // btnParser
+            // 
+            this.btnParser.Location = new System.Drawing.Point(603, 404);
+            this.btnParser.Name = "btnParser";
+            this.btnParser.Size = new System.Drawing.Size(75, 23);
+            this.btnParser.TabIndex = 2;
+            this.btnParser.Text = "Parsing";
+            this.btnParser.UseVisualStyleBackColor = true;
+            this.btnParser.Click += new System.EventHandler(this.btnParser_Click);
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(425, 404);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(75, 23);
+            this.btnShow.TabIndex = 3;
+            this.btnShow.Text = "نمایش";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(12, 404);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "خروج";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 443);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(872, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(872, 465);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.btnParser);
+            this.Controls.Add(this.btnCrawl);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "فرم اصلی";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnCrawl;
+        private System.Windows.Forms.Button btnParser;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+    }
+}
+
---
+++ Final Project/Form1.cs
@@ -0,0 +1,454 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using HtmlAgilityPack;
+using System.Net;
+
+namespace Final_Project
+{
+    public partial class frmMain : Form
+    {
+        static List<string> url_list = new List<string>();
+        static object lock1 = new object();
+        int x = 1;
+
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader(@"E:\mehrnews\dataBase.txt");
+            string s = sr.ReadToEnd();
+            sr.Close();
+
+            DataTable dt = new DataTable();
+
+            dt.Columns.Add("ردیف");
+            dt.Columns.Add("لینک خبر");
+            dt.Columns.Add("لینک عکس");
+            dt.Columns.Add("روز و تاریخ");
+            dt.Columns.Add("عنوان خبر");
+            dt.Columns.Add("معرفی خبر");
+            dt.Columns.Add("شناسه،روز،تاریخ و ساعت");
+            dt.Columns.Add("موضوع");
+            dt.Columns.Add("چکیده خبر");
+            dt.Columns.Add("متن خبر").MaxLength.ToString();
+
+            string[] row = s.Split('$');
+
+            for (int i = 0; i < row.Length - 1; i++)
+            {
+                string[] coulmn = row[i].Split('#');
+                dt.Rows.Add(coulmn[0], coulmn[1], coulmn[2], coulmn[3], coulmn[4], coulmn[5], coulmn[6], coulmn[7], coulmn[8], coulmn[9]);
+            }
+            dataGridView1.DataSource = dt;
+        }
+
+        private void btnCrawl_Click(object sender, EventArgs e)
+        {
+            toolStripProgressBar1.Value = 0;
+            toolStripStatusLabel1.Text = "Crawling Web Pages...";
+            crawl();
+        }
+
+        private void btnParser_Click(object sender, EventArgs e)
+        {
+            toolStripProgressBar1.Value = 0;
+            toolStripStatusLabel1.Text = "Parsing Data...";
+            parsing();
+        }
+
+
+        public void crawl()
+        {
+            for (int i = 1; i <= 1; i++)
+            {
+                string url = "http://www.mehrnews.com/page/archive.xhtml?pi=" + i + "&ms=0";
+                url_list.Add(url);
+            }
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    Thread t = new Thread(Threading);
+            //    t.Start();
+            //    toolStripProgressBar1.Value++;
+            //}
+            string savepath = @"E:\mehrnews\";
+            for (int i = 0; i < url_list.Count; i++)
+            {
+                webcrawling(url_list[i], savepath + (i + 1) + ".html", i + 1);
+                toolStripProgressBar1.Value++;
+            }
+
+            while (toolStripProgressBar1.Value < 100)
+            {
+                toolStripProgressBar1.Value++;
+            }
+            if (toolStripProgressBar1.Value == 100)
+                toolStripStatusLabel1.Text = "Crawling Finished";
+
+        }
+
+
+        public void parsing()
+        {
+
+            List<string[]> list = new List<string[]>();
+            string[] HtmlFiles = Directory.GetFiles(@"e:\mehrnews\", "*.html");
+            for (int i = 0; i < HtmlFiles.Length; i++)
+            {
+                string file = HtmlFiles[i];
+                HtmlAgilityPack.HtmlDocument Doc1 = new HtmlAgilityPack.HtmlDocument();
+                Doc1.Load(file, Encoding.UTF8);
+                var Temp = Doc1.DocumentNode.SelectNodes("//div");
+                HtmlNode Root = null;
+                if (Temp != null)
+                {
+                    for (int j = 0; j < Temp.Count; j++)
+                    {
+                        for (int k = 0; k < Temp[j].Attributes.Count; j++)
+                        {
+                            if (Temp[j].Attributes[k].Name == "class" && Temp[j].Attributes[k].Value == "third-news")
+                            {
+                                Root = Temp[j];
+                                break;
+                            }
+                        }
+                        if (Root != null)
+                            break;
+                    }
+                }
+                if (Root != null)
+                {
+                    List<HtmlNode> li_list = new List<HtmlNode>();
+                    Queue<HtmlNode> Q = new Queue<HtmlNode>();
+                    Q.Enqueue(Root);
+                    HtmlNode ul = null;
+                    while (Q.Count > 0)
+                    {
+                        for (int j = 0; j < Q.Peek().ChildNodes.Count; j++)
+                        {
+                            Q.Enqueue(Q.Peek().ChildNodes[j]);
+                        }
+                        Q.Dequeue();
+                        while (Q.Count > 0)
+                        {
+                            if (Q.Peek().Name == "ul")
+                                ul = Q.Peek();
+                            Q.Dequeue();
+                        }
+                    }//ul tag found
+                    Q.Enqueue(ul);
+                    while (Q.Count > 0)
+                    {
+                        int s = 1;
+                        if (Q.Peek().Name == "li")
+                        {
+                            for (int j = 0; j < Q.Peek().Attributes.Count; j++)
+                            {
+                                if (Q.Peek().Attributes[j].Name == "class" && Q.Peek().Attributes[j].Value == "clearfix")
+                                {
+                                    li_list.Add(Q.Peek());
+                                    s = 0;
+                                    break;
+                                }
+                            }
+
+                        }
+                        if (s == 1)
+                        {
+                            //MessageBox.Show(Q.Peek().ChildNodes.Count.ToString());
+                            for (int j = 0; j < Q.Peek().ChildNodes.Count; j++)
+                            {
+                                Q.Enqueue(Q.Peek().ChildNodes[j]);
+                            }
+                        }
+                        Q.Dequeue();
+                    }
+
+
+
+                    for (int j = 0; j < li_list.Count; j++)
+                    {
+                        string[] Array = new string[10];
+                        Q.Enqueue(li_list[j]);
+                        while (Q.Count > 0)
+                        {
+                            int t = 0;
+                            if (Q.Peek().Name == "a")
+                            {
+                                for (int k = 0; k < Q.Peek().Attributes.Count; k++)
+                                {
+                                    if (Q.Peek().Attributes[k].Name == "href")
+                                    {
+                                        Array[1] = "http://www.mehrnews.com" + Q.Peek().Attributes[k].Value;
+                                        break;
+                                    }
+                                }
+                                t = 1;
+                                //finding image link
+                                Queue<HtmlNode> qt = new Queue<HtmlNode>();
+                                for (int k = 0; k < Q.Peek().ChildNodes.Count; k++)
+                                    qt.Enqueue(Q.Peek().ChildNodes[k]);
+                                HtmlNode span = null;
+                                while (qt.Count > 0)
+                                {
+                                    if (qt.Peek().Name == "span")
+                                    {
+                                        span = qt.Peek();
+                                        break;
+                                    }
+                                    qt.Dequeue();
+                                }
+                                while (qt.Count > 0)
+                                    qt.Dequeue();
+                                qt.Enqueue(span);
+                                while (qt.Count > 0)
+                                {
+                                    int m = 0;
+                                    if (qt.Peek().Name == "img")
+                                    {
+                                        for (int k = 0; k < qt.Peek().Attributes.Count; k++)
+                                        {
+                                            if (qt.Peek().Attributes[k].Name == "src")
+                                            {
+                                                Array[2] = qt.Peek().Attributes[k].Value;
+                                                m = 1;
+                                                break;
+                                            }
+                                        }
+                                    }
+                                    if (m == 1)
+                                        break;
+                                    if (m == 0)
+                                    {
+                                        for (int k = 0; k < qt.Peek().ChildNodes.Count; k++)
+                                            qt.Enqueue(qt.Peek().ChildNodes[k]);
+                                    }
+                                    qt.Dequeue();
+                                }
+
+
+                            }
+                            else if (Q.Peek().Name == "span")
+                            {
+                                Array[3] = Q.Peek().InnerText;
+                                t = 1;
+                            }
+                            else if (Q.Peek().Name == "h3")
+                            {
+                                Queue<HtmlNode> qt = new Queue<HtmlNode>();
+                                for (int k = 0; k < Q.Peek().ChildNodes.Count; k++)
+                                    qt.Enqueue(Q.Peek().ChildNodes[k]);
+                                while (qt.Count > 0)
+                                {
+                                    if (qt.Peek().Name == "a")
+                                    {
+                                        for (int k = 0; k < qt.Peek().Attributes.Count; k++)
+                                        {
+                                            if (qt.Peek().Attributes[k].Name == "href")
+                                                Array[4] = qt.Peek().InnerText;
+                                            break;
+                                        }
+                                    }
+                                    qt.Dequeue();
+                                }
+                                t = 1;
+                            }
+                            else if (Q.Peek().Name == "p")
+                            {
+                                Array[5] = Q.Peek().InnerText;
+                                t = 1;
+
+                            }
+                            if (t == 0)
+                            {
+                                for (int k = 0; k < Q.Peek().ChildNodes.Count; k++)
+                                    Q.Enqueue(Q.Peek().ChildNodes[k]);
+                            }
+                            Q.Dequeue();
+                        }
+
+                        if (Array[2] == null)
+                            Array[2] = "NO IMAGE";
+                        Array[0] = x.ToString();
+                        string[] s = new string[4];
+
+                        s = NewsText(Array[1], x);
+                        x++;
+                        for (int m = 6, n = 0; m <= 9; m++, n++)
+                        {
+                            Array[m] = s[n];
+                        }
+                        list.Add(Array);
+
+
+
+                    }
+
+                }//end of if(Root!=null)
+
+                toolStripProgressBar1.Value++;
+                toolStripStatusLabel1.Text = "Page " + i + " Has Parsed.";
+
+            }//end of for (int i = 0; i < HtmlFiles.Length; i++)
+
+            //********************
+            string SavePath = @"E:\mehrnews\dataBase.txt";
+            StreamWriter sw = new StreamWriter(SavePath);
+
+            for (int j = 0; j < list.Count; j++)
+            {
+                for (int k = 0; k < 9; k++)
+                {
+                    sw.Write(list[j][k] + "#");
+                }
+                sw.Write(list[j][9] + "$");
+                sw.WriteLine();
+            }
+
+            sw.Close();
+            //********************
+
+
+            while (toolStripProgressBar1.Value < 100)
+            {
+                toolStripProgressBar1.Value++;
+            }
+            if (toolStripProgressBar1.Value == 100)
+                toolStripStatusLabel1.Text = "Finished";
+
+        }
+
+        public string[] NewsText(string text, int n)
+        {
+            List<string> w = text.Split('/').ToList();
+
+            try
+            {
+                WebProxy wp = new WebProxy();
+                wp.UseDefaultCredentials = true;
+                WebClient wc = new WebClient();
+                wc.Proxy = wp;
+                string path = @"E:\mehrnews\links\" + n + " . " + w[w.Count - 1] + ".html";
+                wc.DownloadFile(text, path);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            string[] HtmlFiles = Directory.GetFiles(@"e:\mehrnews\links\", "*.html");
+            string file = null;
+            for (int i = 0; i < HtmlFiles.Length; i++)
+            {
+                if (HtmlFiles[i] == ("e:\\mehrnews\\links\\" + n + " . " + w[w.Count - 1] + ".html"))
+                    file = HtmlFiles[i];
+            }
+            HtmlAgilityPack.HtmlDocument Doc1 = new HtmlAgilityPack.HtmlDocument();
+            Doc1.Load(file, Encoding.UTF8);
+            var Temp = Doc1.DocumentNode.SelectNodes("//div");
+            var t = Doc1.DocumentNode.SelectNodes("//span");
+            HtmlNode Root = null, div_meta = null, div_subtitle = null, span = null;
+            string[] s = new string[4];
+            if (Temp != null)
+            {
+                for (int j = 0; j < Temp.Count; j++)
+                {
+                    for (int k = 0; k < Temp[j].Attributes.Count; k++)
+                    {
+                        if (Temp[j].Attributes[k].Name == "class" && Temp[j].Attributes[k].Value == "full-text")
+                        {
+                            Root = Temp[j];
+                        }
+                        else if (Temp[j].Attributes[k].Name == "class" && Temp[j].Attributes[k].Value == "meta")
+                        {
+                            div_meta = Temp[j];
+                        }
+                        else if (Temp[j].Attributes[k].Name == "class" && Temp[j].Attributes[k].Value == "subtitle")
+                        {
+                            div_subtitle = Temp[j];
+                        }
+                    }
+                    if (Root != null)
+                        break;
+                }
+            }
+            if (t != null)
+            {
+                for (int j = 0; j < t.Count; j++)
+                {
+                    for (int k = 0; k < t[j].Attributes.Count; k++)
+                    {
+                        if (t[j].Attributes[k].Name == "class" && t[j].Attributes[k].Value == "intro-text")
+                        {
+                            span = t[j];
+                        }
+                    }
+                }
+            }
+            if (div_meta != null)
+                s[0] = div_meta.InnerText;
+            if (div_subtitle != null)
+                s[1] = div_subtitle.InnerText;
+            if (span != null)
+                s[2] = span.InnerText;
+            if (Root != null)
+            {
+                Queue<HtmlNode> Q = new Queue<HtmlNode>();
+                Q.Enqueue(Root);
+                for (int j = 0; j < Q.Peek().ChildNodes.Count; j++)
+                {
+                    Q.Enqueue(Q.Peek().ChildNodes[j]);
+                }
+                Q.Dequeue();
+                while (Q.Count > 0)
+                {
+                    if (Q.Peek().Name == "p")
+                    {
+                        s[3] += Q.Peek().InnerText;
+                    }
+                    Q.Dequeue();
+                }
+            }
+
+            return s;
+        }
+
+        public void webcrawling(string url, string path, int i)
+        {
+            try
+            {
+                WebProxy wp = new WebProxy();
+                wp.UseDefaultCredentials = true;
+                WebClient wc = new WebClient();
+                wc.Proxy = wp;
+
+               
+                wc.DownloadFile(url, path);
+
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        
+    }
+}
